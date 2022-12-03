447ed960115b6e1c6c77429f263ab3f30423f19d
Sam Mosley <44081942+SMosl@users.noreply.github.com>
2022-12-03 07:52:45 +0000

    Add files via upload

---
+++ Day 1/Day1.py
@@ -0,0 +1,23 @@
+def main1():
+	with open("./input.txt", "r") as f:
+		content = f.read().split('\n\n')
+
+	cals = []
+	for elf in content:
+		cals.append(sum([int(x) for x in elf.split('\n')]))
+	max_cals = max(cals)
+
+	return(max_cals,cals)
+
+
+def main2(cals):
+	sorted_list = sorted(cals, reverse=True)
+	return(sum([x for x in sorted_list[:3]]))
+
+
+if __name__ == "__main__":
+	part1 = main1()
+	print(part1[0])
+
+	part2 = main2(part1[1])
+	print(part2)
---
+++ Day 3/Day3.py
@@ -0,0 +1,28 @@
+def main1():
+	with open("./input.txt", "r") as f:
+		final_sum = 0
+		while(len(line := f.readline()) != 0):
+			x = int(len(line.strip())/2)
+			rucksack = [line[:x], line[x:]]
+			common = ord(''.join(set(rucksack[0]).intersection(rucksack[1])))
+			if common < 91:
+				final_sum += common - 38
+			else:
+				final_sum += common - 96
+	print(final_sum)
+
+def main2():
+	final_sum = 0
+	with open("./input.txt", "r") as f:
+		content = f.read().splitlines()
+	for i in range(int(len(content)/3)):
+		common = ord(''.join(set(content[3*i]).intersection(content[3*i+1]).intersection(content[3*i+2])))
+		if common < 91:
+			final_sum += common - 38
+		else:
+			final_sum += common - 96		
+	print(final_sum)
+
+if __name__ == "__main__":
+	part1 = main1()
+	part2 = main2()
