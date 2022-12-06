4a10e135c82dca8912a76f2ed8235f32f370f3ce
Sam Mosley <sam.mosley@gmail.com>
2022-12-06 08:38:36 +0000

    Update Day6.py

--- Day 6/Day6.py
+++ Day 6/Day6.py
@@ -3,10 +3,11 @@ import os
 def main(part):
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	with open(f"{dir_path}/input.txt", "r") as f:
+		line = f.readline()
+
 		# Part 1 has markers of length 4, part 2 has markers of length 14
 		num_chars = (10 * (part - 1)) + 4
-		
-		line = f.readline()
+
 		for position in range(num_chars, len(line)):
 			potential_marker = line[position - num_chars : position]
 			# Check if the characters in the current potential marker are unique to one another
