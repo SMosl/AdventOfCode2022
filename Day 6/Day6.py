47de98be91caf80ff7b74481e0db5f7b00b2e840
Sam Mosley <sam.mosley@gmail.com>
2022-12-06 06:38:25 +0000

    Update Day6.py

--- Day 6/Day6.py
+++ Day 6/Day6.py
@@ -1,19 +1,15 @@
 import os
-import re
 
 def main(part):
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	with open(f"{dir_path}/input.txt", "r") as f:
 		# Part 1 has markers of length 4, part 2 has markers of length 14
-		if part == 1:
-			num_chars = 4
-		else:
-			num_chars = 14
+		num_chars = (10 * (part - 1)) + 4
 		
 		line = f.readline()
 		position = num_chars
 		while(position < len(line)):
-			potential_marker = line[position - num_chars :position]
+			potential_marker = line[position - num_chars : position]
 			# Check if the characters in the current potential marker are unique to one another
 			if len(set(potential_marker)) == len(potential_marker):
 				return(position, potential_marker)
