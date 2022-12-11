d96e697d96fda27c67c707e65fbe1bda2fdc60b0
Sam Mosley <sam.mosley@gmail.com>
2022-12-11 07:21:26 +0000

    Solve Day 5

---
+++ Day 05/Day5.py
@@ -0,0 +1,58 @@
+import os
+import re
+
+def main1():
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+	with open(f"{dir_path}/input.txt", "r") as f:
+		# read the input and split into the two sections
+		[stacks_raw, instructions] = f.read().split('\n\n')
+		stacks_split = stacks_raw.splitlines()
+		num_cols = int(stacks_split[-1].split()[-1])
+		
+		# populate the stacks as a list of strings
+		stacks = ['' for _ in range(num_cols)]
+		for col in range(len(stacks_split)-1):
+			for cargo in range(num_cols):
+				val = stacks_split[col][4*cargo + 1 : 4*cargo + 2]
+				if val != ' ':
+					stacks[cargo] += val
+
+		# find the relevant integers from the list of instructions and apply the moves
+		instructions = [[int(i) for i in re.findall(r'\d+', line)] for line in instructions.splitlines()]
+		for instruction in instructions:
+			for x in range(instruction[0]):
+				stacks[instruction[2] - 1] = stacks[instruction[1] - 1][0] + stacks[instruction[2] - 1]
+				stacks[instruction[1] - 1] = stacks[instruction[1] - 1][1:]
+
+		message = ''
+		for stack in stacks:
+			message += stack[0][0]
+		print(message)
+
+def main2():
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+	with open(f"{dir_path}/input.txt", "r") as f:
+		[stacks_raw, instructions] = f.read().split('\n\n')
+		stacks_split = stacks_raw.splitlines()
+		num_cols = int(stacks_split[-1].split()[-1])
+
+		stacks = ['' for _ in range(num_cols)]
+		for col in range(len(stacks_split)-1):
+			for cargo in range(num_cols):
+				val = stacks_split[col][4*cargo + 1 : 4*cargo + 2]
+				if val != ' ':
+					stacks[cargo] += val
+
+		instructions = [[int(i) for i in re.findall(r'\d+', line)] for line in instructions.splitlines()]
+		for instruction in instructions:
+			stacks[instruction[2] - 1] = stacks[instruction[1] - 1][:instruction[0]] + stacks[instruction[2] - 1]
+			stacks[instruction[1] - 1] =  stacks[instruction[1] - 1][instruction[0]:]
+
+		message = ''
+		for stack in stacks:
+			message += stack[0][0]
+		print(message)
+
+if __name__ == "__main__":
+	part1 = main1()
+	part2 = main2()
